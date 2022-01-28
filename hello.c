79fb4803d47d5587f343eb524640939ee32f078a
Jessica Chavez <jchav198@sledge.cs.ucr.edu>
2022-01-27 22:43:37 -0800

    commit on main

---
+++ hello.c
@@ -0,0 +1,12 @@
+//
+// Created by Jessica Chavez on 1/19/2022.
+//
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+    printf(1, "my first hello world program");
+    exit();
+}
