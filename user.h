e943574527d5e5bf3dc61acbcf699dddf80767f4
sourshoes <86706385+sourshoes@users.noreply.github.com>
2022-01-28 13:49:58 -0800

    Update user.h

--- user.h
+++ user.h
@@ -3,7 +3,7 @@ struct rtcdate;
 
 // system calls
 int fork(void);
-int exit(int) __attribute__((noreturn));
+void exit(int) __attribute__((noreturn));
 int wait(int *);
 int waitpid(int, int*, int);
 int pipe(int*);
